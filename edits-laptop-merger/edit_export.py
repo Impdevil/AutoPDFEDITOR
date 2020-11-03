7d93c362df294801b51a518e0b27bdd8b6909c84
Arran Blows <tartankavujr@gmail.com>
2020-11-03 18:32:41 +0000

    laptopedits1

---
+++ edits-laptop-merger/edit_export.py
@@ -0,0 +1,95 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter,PdfFileMerger
+import datetime as dt
+from pathlib import Path
+import os
+import xmltodict
+
+
+
+def Find_Pdf(date):
+    print("find file")
+    test_Fname = "test for arran.pdf"
+    
+    if int(date.month) < 10:
+        month = "0"+str(date.month)
+    else:
+        month = str(date.month)
+    if date.day < 10:
+        day = "0" + str(date.day)
+    else:
+        day = str(date.day)
+       
+    filename =  "pdfs/"+str(date.month) + "/" +str(day)+ str(month)+" mbeb.pdf"
+    
+    print(filename)
+    try:
+        pdf = PdfFileReader(str(filename))
+    
+        print(pdf.getNumPages())
+        print("will convert file from pdf to odt for title edit")
+    except:
+        print("invalid date or no file found")
+        pdf = False
+    return pdf
+
+
+def Edit_Pdf(pdf_edit ,date):
+    
+    pdf_numpages = pdf_edit.getNumPages()
+    currentyear = dt.datetime.utcnow()
+    removepages = pdf_edit.getNumPages() - (((currentyear.year) - date.year)*2 +2)
+    print("removing page 6 though " +  str(removepages))
+    print(str(pdf_numpages) +" + "+ str(removepages) +">= "+ str(pdf_numpages))
+    if pdf_numpages - removepages >= pdf_numpages - 6:
+        print("not enough pages to edit, exporting anyway")
+        pdf_writer= PdfFileWriter()
+        for n in range(0,pdf_numpages):
+            page = pdf_edit.getPage(n)
+            pdf_writer.addPage(page)
+        return pdf_writer
+    else:
+        pdf_writer = PdfFileWriter()
+        for n in range(0,4):
+            page = pdf_edit.getPage(n)
+            pdf_writer.addPage(page)
+        for n in range(removepages,pdf_numpages):
+            page = pdf_edit.getPage(n)
+            pdf_writer.addPage(page)
+
+        print( "original pdf lenght "+ str(pdf_numpages)+ " | new PDF length:"+str(pdf_writer.getNumPages()))
+        
+        return pdf_writer
+    
+
+def Export_Pdf(batch_num, ready_pdf,date):
+        if not os.path.exists("batches/"+batch_num):
+            os.mkdir("batches/" + batch_num)
+            print("Making Batch folder, and exporting")
+        else:
+            print("batch folder already exists exporting to file")
+        with Path("batches/"+batch_num,str(date.year)+str(date.month)+str(date.day)+"-Title Edit Required.pdf").open(mode="wb") as output_File:
+            ready_pdf.write(output_File)
+
+def readXML(xmlfilepath):  
+    with open(xmlfilepath) as fd:
+        curr_job = xmltodict.parse(fd.read())
+        print(curr_job["Batch"]["Calendar"]["JobTicket"])
+    
+
+    return curr_job
+
+def XML_getPDF(data):
+    date = data["Batch"]["Calendar"]["StockItem"]
+    temp = ""
+    for x in range(0,8):
+        temp = temp + date[x]
+    date = temp
+    date = dt.datetime.strptime(date,"%Y%d%m")
+    print(date)
+    pdf = list()
+    pdf.append(Find_Pdf(date))
+    pdf.append(date)
+    return pdf
+
+print("importing methods")
+
---
+++ edits-laptop-merger/main.py
@@ -0,0 +1,63 @@
+
+import edit_export
+import re
+import datetime as dt
+import os
+
+from edit_export import Edit_Pdf, Export_Pdf, readXML
+job_done = False
+
+
+batch_list = ""
+
+while job_done != True:
+
+    batch_number = input("Following commands: Type digits of manual batch,\nType auto for automatically running xml\n Or type quit to exit\n")
+    curr_batch = batch_number
+    if batch_number == "auto":
+        for file in os.listdir("batchxml"):
+            if file.endswith(".xml"):
+                curr_job = edit_export.readXML(os.path.join("batchxml",file))
+                curr_pdf = edit_export.XML_getPDF(curr_job)
+                curr_jobDate = curr_pdf[1]
+                curr_pdf = curr_pdf[0]
+                curr_pdf = edit_export.Edit_Pdf(curr_pdf,curr_jobDate)
+                edit_export.Export_Pdf(curr_job["Batch"]["Calendar"]["JobTicket"],curr_pdf,curr_jobDate)
+
+                #os.remove(file)
+
+            
+    elif batch_number == "quit":
+        print("completed batches: " + batch_list)
+        job_done = True
+    elif batch_number.isdigit() == True:
+        while curr_batch == batch_number:
+            waiting = True
+
+            curr_job= input("what is the day, month, and year(format = DD/MM/YYYY)? \n")
+            #try:
+            if True:     
+                print(curr_job)
+                curr_jobDate = dt.datetime.strptime(curr_job, "%d/%m/%Y")
+                curr_pdf = edit_export.Find_Pdf(curr_jobDate)
+                if curr_pdf != False:
+                    curr_pdf = edit_export.Edit_Pdf(curr_pdf, curr_jobDate)
+                    if curr_pdf != False:
+                        edit_export.Export_Pdf(curr_batch,curr_pdf,curr_jobDate)
+                    while waiting == True:
+                        status = input("Add more to this batch? Y/N  ")
+                        if status == "N" or status == "n":
+                            batch_number = ""
+                            batch_list = batch_list + "," + curr_batch
+                            waiting = False
+                        if status == "y" or status == "Y":
+                            print("new file")
+                            waiting = False
+                else:
+                    print("please use a new date")
+            #except:
+            else:
+                print("invaild date, please try again.")
+
+        
+    
